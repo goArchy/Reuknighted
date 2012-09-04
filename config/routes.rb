3d991443d51ddb5fdc658df1c4c1e41fe7773187
Guy Archibald <goarchib83@gmail.com>
2012-09-04 04:46:51 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Reuknighted::Application.routes.draw do
 
-  root to: 'static_pages#home'
+  get 'users/new'
 
+  root to: 'static_pages#home'
+  
+  match '/signup',  to: 'users#new'
+  
   match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
+  match '/contact', to: 'static_pages#contact' 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
